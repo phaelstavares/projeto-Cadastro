c80b5a1a4387f6fa9fe53d0c7f666731aa15e8da
Raphael Tavares <77020757+phaelstavares@users.noreply.github.com>
2024-04-09 16:15:10 -0300

    estado no react

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import React from "react";
-import { Container, Imagem, ContainerItens, H1, InputLabel, Input, Button } from "./styles"
+import React, { useState } from "react";
+import { Container, Imagem, ContainerItens, H1, InputLabel, Input, Button, User } from "./styles"
 import People from "./assets/img-pessoas-inicio.svg"
 import Seta from "./assets/seta-direita.svg"
+import Lixeira from "./assets/lixeira.svg"
 
 // JSX
 export default function App ()  {
+  // React Hooks -> Ferramentas auxiliares
+  const [ users, setUsers ] = useState([])
+
+
+  function addNewUser() {
+    setUsers([{ id: Math.random(), nome: "Raphael", idade: 19}])
+  }
 
   return (
     <Container>
@@ -19,8 +27,17 @@ export default function App ()  {
           <InputLabel>Idade</InputLabel>
           <Input placeholder="Idade"></Input>
 
-          <Button>Cadastrar <img alt="seta" src={Seta}/></Button>
+          <Button onClick={addNewUser}>Cadastrar <img alt="seta" src={Seta}/></Button>
 
+          <ul>
+            { users.map(user => (
+              <User key={user.id}>
+              <p>{user.nome}</p> <p>{user.idade}</p>
+              <button><img src={Lixeira} alt="lixeira"/></button>
+              </User>
+            ))
+            }
+          </ul>
         </ContainerItens>
     </Container>
   )
--- src/styles.js
+++ src/styles.js
@@ -24,8 +24,6 @@ export const ContainerItens = styled.div `
     padding: 50px 36px;
     display: flex;
     flex-direction: column;
-
-    height: 100%;
 `
 
 export const H1 = styled.h1 `
@@ -95,5 +93,37 @@ export const Button = styled.button `
 
     &:active {
         opacity: 0.6;
+    }
+`
+
+export const User = styled.li `
+    margin-top: 20px;
+    
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+
+    background: rgba(255, 255, 255, 0.25);
+    box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+    border-radius: 14px;
+
+    width: 342px;
+    height: 58px;
+
+    border: none;
+    outline: none;
+
+    p {
+        font-style: normal;
+        font-weight: normal;
+        font-size: 20px;
+        line-height: 28px;
+        color: #ffffff;
+    }
+    
+    button {
+        background: none;
+        border: none;
+        cursor: pointer;
     }
 `
